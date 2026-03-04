7c6b62e7deeb23c0d19d3347d5bc62c8712650f6
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-03-04 09:42:06 +0000

    Update domty.js

--- providers/domty.js
+++ providers/domty.js
@@ -1,21 +1,19 @@
-const PROVIDER_NAME = "DOMTY";
+exports.name = "DOMTY";
 
-async function getStreams(tmdbId, mediaType, season, episode) {
+exports.getStreams = async function (tmdbId, mediaType, season, episode) {
 
   const streams = [];
 
   const url =
     mediaType === "movie"
-      ? `https://vidsrc.to/embed/movie/${tmdbId}`
-      : `https://vidsrc.to/embed/tv/${tmdbId}/${season}/${episode}`;
+      ? "https://vidsrc.to/embed/movie/" + tmdbId
+      : "https://vidsrc.to/embed/tv/" + tmdbId + "/" + season + "/" + episode;
 
   streams.push({
-    name: PROVIDER_NAME,
+    name: "DOMTY",
     title: "Auto",
     url: url
   });
 
   return streams;
-}
-
-module.exports = { getStreams };
+};
